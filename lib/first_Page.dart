124f76562ca6c4b47e29797db41d9e94ee050420
dabhinirav41289 <dabhinirav53@gmail.com>
2023-05-29 11:37:10 +0530

    to create second page

---
+++ lib/first_Page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPage createState() => _FirstPage();
+}
+
+class _FirstPage extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
